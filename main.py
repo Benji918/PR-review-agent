e1254e484de42d976f30df351dcc7880453c838a
Benjamin <kodiugos@gmail.com>
2025-05-19 17:15:25 +0100

    chore:project setup

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request, HTTPException, Response, status
+import uvicorn
+from fastapi.responses import ORJSONResponse
+
+app = FastAPI(
+    title="PR-review-agent",
+    description="A simple agent for PR reviews",
+    version="1.0",
+    default_response_class=ORJSONResponse,
+)
+
+@app.get("/", tags=["Home"])
+async def get_root(request: Request):
+    """Base endpoint"""
+    return "Welcome to PR-review-agent API",
+
+
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host="127.0.0.1", port=4000, reload=True)
+
