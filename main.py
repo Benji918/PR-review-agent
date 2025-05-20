a7326b5bbbad721ab00e4748c2329ff4d15ea563
Benjamin <kodiugos@gmail.com>
2025-05-20 12:28:11 +0100

    chore: registered project endpoints

--- main.py
+++ main.py
@@ -22,5 +22,5 @@ async def get_root(request: Request):
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host="127.0.0.1", port=4001, reload=True)
+    uvicorn.run('main:app', host="127.0.0.1", port=3000, reload=True)
 
